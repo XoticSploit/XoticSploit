bf56e56da6ce358607ecc8f6b3024789571f2739
XoticSploit <bigdaddydylan.01@gmail.com>
2023-10-08 16:52:58 -0500

    xoticsploit 2.0 notice

--- XoticSploit/index.js
+++ XoticSploit/index.js
@@ -65,3 +65,32 @@ $(document).ready(function(){
       });
   });
   
+  
+  
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+  
